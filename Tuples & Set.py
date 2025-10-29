ec606c49efd34d0537367b32cce16c30fd19c244
rifarizqia10224136 <rifa10224136@student.sttcipasung.ac.id>
2025-10-29 08:12:14 +0700

    tuples dan set
    kelompok 3 
    https://youtu.be/BWQn2TQqvY8?si=WTt9nwNQPQo4kHCQ
    - by rifa rizqia (10224136)

---
+++ Tuples & Set.py
@@ -0,0 +1,25 @@
+# List
+# menggunakan kurung siku
+data_list = [10,2,4,3,2]
+print(data_list)
+
+# tuples
+data_tuples = (7,8,9,10)
+print(data_tuples)
+print(data_tuples[3])
+
+# tidak bisa dilakukan
+# data_tuples[1] = "kiaw"
+# data_tuples.append(1)
+
+# sets (himpunan)
+# bisa di hitung, append dll
+# menggunakan kurung kurawa
+data_sets = {10,4,3,2,4,7,6,5}
+print(data_sets)
+
+# tidak bisa mengambil data dari sets (indeks)
+# print(data_sets[1])
+
+
+
