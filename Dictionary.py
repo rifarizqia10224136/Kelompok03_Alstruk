305dacefba2c4c0d9cbbc649962a9cdacefb970b
rifarizqia10224136 <rifa10224136@student.sttcipasung.ac.id>
2025-10-29 08:38:26 +0700

    dictionary
    kelompok 3
    https://youtu.be/Z0hbtSr-Oaw?si=zL-cNPKkeAN9GbJH
    - rifa rizqia (10224136)

---
+++ Dictionary.py
@@ -0,0 +1,22 @@
+# list -> array, mengakses dengan 
+# menggunakan index
+
+data_list = ['zidan', 'kiaw', 'serli']
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+    'key':'value',
+    'zd':'zidan',
+    'kw':'kiaw',
+    'sl':'serli',
+    'nmbr':100,
+    'list':data_list,
+}
+
+print(data_dict['sl'])
+print(data_dict['nmbr'])
+print(data_dict['list'])
